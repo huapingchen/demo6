068681c1051aa77068073217345f6c32fdaa9556
huaping.vip <huaping.vip@gmail.com>
2021-10-02 15:22:43 +0800

    test

---
+++ src/com/chp/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.chp.demo;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello chen Huaping");
+        System.out.println("Hello chen Huaping2");
+        System.out.println("Hello chen Huaping3");
+        System.out.println("Hello chen Huaping4");
+    }
+}
---
+++ src/com/chp/demo/Testfordemo.java
@@ -0,0 +1,10 @@
+package com.chp.demo;
+
+/**
+ * Created by Chen Huaping on 2021/10/2.
+ */
+public class Testfordemo {
+    public void test() {
+        System.out.println("Hello");
+    }
+}
