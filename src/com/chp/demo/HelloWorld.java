80305ec32db4d22947a0074ff7c462fe7af557d2
huaping.vip <huaping.vip@gmail.com>
2021-10-02 20:53:47 +0800

    test

--- src/com/chp/demo/HelloWorld.java
+++ src/com/chp/demo/HelloWorld.java
@@ -3,7 +3,7 @@ package com.chp.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello chen Huaping");
-        System.out.println("Hello chen Huaping2");
+        System.out.println("Hello chen Huaping26");
         System.out.println("Hello chen Huaping33");
     }
 }
